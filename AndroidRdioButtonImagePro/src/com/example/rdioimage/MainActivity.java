3bda9e7feac38e3b67152258755185140528314e
MV <MV@MV-PC>
2016-02-22 16:22:50 +0800

    AndroidRadioButton图片处理，自定义RadioButton图片大小，可以在xml中设置drawableTop等显示的图片大小属性。

---
+++ AndroidRdioButtonImagePro/src/com/example/rdioimage/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.rdioimage;
+
+import com.example.androidrdiobuttonimagepro.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+/**
+ * 调整RadioButton的图片大小
+ * @author MV
+ *
+ */
+public class MainActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
